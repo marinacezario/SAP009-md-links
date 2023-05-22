45bae143cdee9270c1cd1d81361219f9e44cfb0d
marinacezario <marina.cezario@gmail.com>
2023-05-22 14:00:51 -0300

    instalaçao fetch, atualizacao json

--- src/links-handler.js
+++ src/links-handler.js
@@ -1,76 +1,76 @@
 function extractLinks (fileData) {
-    const mdText = fileData.data
-    const regExLink = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
-    const linkMatcher = mdText.match(regExLink)
-  
-    if (linkMatcher !== null) {
-      const links = linkMatcher.map(link => {
-        const removePunctuation = link.replace(/.$/, '').replace(/^./, '')
-        const splitEx = removePunctuation.split('](')
-  
-        const linksObj = {
-          file: fileData.file,
-          text: splitEx[0],
-          href: splitEx[1]
-        }
-  
-        return linksObj
-      })
-      console.log(links)
-      return links
-    }
-  }
-  
-  function getLinks (files) {
-    const filesArr = []
-    filesArr.push(files)
-    return new Promise((resolve, reject) => {
-      const linksArr = filesArr.flatMap(extractLinks)
-        .filter((link) => link)
-      resolve(linksArr)
-    })
-  }
-  
-  function validateLinks (linksArr) {
-    return Promise.all(
-      linksArr.map(element => {
-        return fetch(element.href)
-          .then(linksObj => {
-            if (linksObj.ok !== undefined && linksObj.status !== undefined) {
-              const fetchLinkObj = { ...element, status: linksObj.status, ok: linksObj.ok }
-              return fetchLinkObj
-            } else {
-              throw new Error('Invalid fetch response')
-            }
-          })
-          .catch(err => ({ ...element, status: err, ok: false }))
-      }))
-  }
-  
-  function linkStats (linksArr) {
-    return new Promise((resolve) => {
-      const hrefList = []
-      let broken = 0
-      linksArr.forEach(element => {
-        hrefList.push(element.href)
-        if (element.ok === false) {
-          broken++
-        };
-      })
-  
-      const uniqueLinks = new Set(hrefList)
-  
-      const objStats = {
-        total: hrefList.length,
-        unique: uniqueLinks.size,
-        broken
+  const mdText = fileData.data
+  const regExLink = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+  const linkMatcher = mdText.match(regExLink)
+
+  if (linkMatcher !== null) {
+    const links = linkMatcher.map(link => {
+      const removePunctuation = link.replace(/.$/, '').replace(/^./, '')
+      const splitEx = removePunctuation.split('](')
+
+      const linksObj = {
+        file: fileData.file,
+        text: splitEx[0],
+        href: splitEx[1]
       }
-      resolve(objStats)
+
+      return linksObj
     })
+    console.log(links)
+    return links
   }
-  
-  module.exports = {
-    getLinks,
-    validateLinks,
-    linkStats
-  }
+}
+
+function getLinks (files) {
+  const filesArr = []
+  filesArr.push(files)
+  return new Promise((resolve, reject) => {
+    const linksArr = filesArr.flatMap(extractLinks)
+      .filter((link) => link)
+    resolve(linksArr)
+  })
+}
+
+function validateLinks (linksArr) {
+  return Promise.all(
+    linksArr.map(element => {
+      return fetch(element.href)
+        .then(linksObj => {
+          if (linksObj.ok !== undefined && linksObj.status !== undefined) {
+            const fetchLinkObj = { ...element, status: linksObj.status, ok: linksObj.ok }
+            return fetchLinkObj
+          } else {
+            throw new Error('Invalid fetch response')
+          }
+        })
+        .catch(err => ({ ...element, status: err, ok: false }))
+    }))
+}
+
+function linkStats (linksArr) {
+  return new Promise((resolve) => {
+    const hrefList = []
+    let broken = 0
+    linksArr.forEach(element => {
+      hrefList.push(element.href)
+      if (element.ok === false) {
+        broken++
+      };
+    })
+
+    const uniqueLinks = new Set(hrefList)
+
+    const objStats = {
+      total: hrefList.length,
+      unique: uniqueLinks.size,
+      broken
+    }
+    resolve(objStats)
+  })
+}
+
+module.exports = {
+  getLinks,
+  validateLinks,
+  linkStats
+}
